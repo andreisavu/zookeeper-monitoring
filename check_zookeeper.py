d5cb1b63fb9b56c59ee1391d6f27450ba2a1a028
Andrei Savu <savu.andrei@gmail.com>
2010-06-19 19:52:19 +0300

    mntr 4letter word output parser and tests

--- check_zookeeper.py
+++ check_zookeeper.py
@@ -2,25 +2,83 @@
 """ Check Zookeeper Cluster
 
 Generic monitoring script that could be used with multiple platforms.
+
+It requires ZooKeeper 3.4.0 or greater or patch ZOOKEEPER-744
 """
 
 import sys
+import socket
 
+from StringIO import StringIO
 from optparse import OptionParser
+
+__version__ = (0, 1, 0)
+
+class ZooKeeperServer(object):
+
+    def __init__(self, host='localhost', port='2181', timeout=1):
+        self._address = (host, int(port))
+        self._timeout = timeout
+
+    def get_stats(self):
+        """ Get ZooKeeper server stats as a map """
+        s = socket.socket()
+        s.settimeout(self._timeout)
+
+        s.connect(self._address)
+        s.send('mntr')
+
+        data = s.recv(2048)
+        s.close()
+
+        return self._parse(data)
+
+    def _parse(self, data):
+        """ Parse the output from the 'mntr' 4letter word command """
+        h = StringIO(data)
+        
+        result = {}
+        for line in h.readlines():
+            try:
+                key, value = self._parse_line(line)
+                result[key] = value
+            except ValueError:
+                pass # ignore broken lines
+
+        return result
+
+    def _parse_line(self, line):
+        try:
+            key, value = map(str.strip, line.split('\t'))
+        except ValueError:
+            raise ValueError('Found invalid line: %s' % line)
+
+        if not key:
+            raise ValueError('The key is mandatory and should not be empty')
+
+        try:
+            value = int(value)
+        except (TypeError, ValueError):
+            pass
+
+        return key, value
 
 def main():
     opts, args = parse_cli()
 
-    # find the leader
-    
-    # send the "mntr" command and read output
+    stats = {}
+    for host, port in opts.servers:
+        zk = ZooKeeperServer(host, port)
+        stats["%s:%s" % (host, port)] = zk.get_stats()
 
-    # parse output and get only the specified key
+    print stats
 
-    # select formatter and output
+
+def get_version():
+    return '.'.join(map(str, __version__))
 
 def parse_cli():
-    parser = OptionParser()
+    parser = OptionParser(usage='./check_zookeeper.py <options>', version=get_version())
 
     parser.add_option('-s', '--servers', dest='servers', 
         help='a list of SERVERS', metavar='SERVERS')
@@ -28,16 +86,23 @@ def parse_cli():
     parser.add_option('-k', '--key', dest='key', 
         help='what KEY to analyze', metavar='KEY')
 
-    parser.add_option('-t', '--target', dest='target', 
-        help='target platform', metavar='TARGET')
-
     parser.add_option('-w', '--warning', dest='warning', 
         help='WARNING level', metavar='WARNING')
 
     parser.add_option('-c', '--critical', dest='critical',
         help='CRITICAL level', metavar='CRITICAL')
 
-    return parser.parse_args()
+    parser.add_option('-o', '--output', dest='output', 
+        help='output HANDLER: nagios, ganglia, cacti', metavar='HANDLER')
+
+    opts, args = parser.parse_args()
+
+    if opts.servers is None:
+        parser.error('The list of servers is mandatory')
+
+    opts.servers = [s.split(':') for s in opts.servers.split(',')]
+
+    return (opts, args)
 
 if __name__ == '__main__':
     sys.exit(main())
--- test.py
+++ test.py
@@ -2,8 +2,56 @@
 
 import unittest
 
+from check_zookeeper import ZooKeeperServer
+
+ZK_MNTR_OUTPUT = """zk_version\t3.4.0--1, built on 06/19/2010 15:07 GMT
+zk_avg_latency\t1
+zk_max_latency\t132
+zk_min_latency\t0
+zk_packets_received\t640
+zk_packets_sent\t639
+zk_outstanding_requests\t0
+zk_server_state\tfollower
+zk_znode_count\t4
+zk_watch_count\t0
+zk_ephemerals_count\t0
+zk_approximate_data_size\t27
+zk_open_file_descriptor_count\t22
+zk_max_file_descriptor_count\t1024
+"""
+
+ZK_MNTR_OUTPUT_WITH_BROKEN_LINES = """zk_version\t3.4.0
+zk_avg_latency\t23
+broken-line
+
+"""
+
 class TestCheckZookeeper(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.zk = ZooKeeperServer()
+    
+    def test_parse_valid_line(self):
+        key, value = self.zk._parse_line('something\t5')
+
+        self.assertEqual(key, 'something')
+        self.assertEqual(value, 5)
+
+    def test_parse_line_raises_exception_on_invalid_output(self):
+        invalid_lines = ['something', '', 'a\tb\tc', '\t1']
+        for line in invalid_lines:
+            self.assertRaises(ValueError, self.zk._parse_line, line)
+
+    def test_parser_on_valid_output(self):
+        data = self.zk._parse(ZK_MNTR_OUTPUT)
+
+        self.assertEqual(len(data), 14)
+        self.assertEqual(data['zk_znode_count'], 4)
+        
+    def test_parse_should_ignore_invalid_lines(self):
+        data = self.zk._parse(ZK_MNTR_OUTPUT_WITH_BROKEN_LINES)
+
+        self.assertEqual(len(data), 2)
 
 if __name__ == '__main__':
     unittest.main()
